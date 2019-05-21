867406e7200f86b83ec06ef50559ad613e20e2d7
Angelica Santos <angelicasantos@zipcoders-MacBook-Pro-2.local>
2019-05-21 17:12:42 -0400

    problem solved.

--- Main.java
+++ Main.java
@@ -1,9 +1,17 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner; 
 public class Main {
 
     public static void main(String[] args ){
+        Scanner in = new Scanner(System.in); // declares scanner 
+        System.out.println("what is your name? "); // prompt user for name 
+        String name = in.nextLine(); //reads input and assigns to name 
+       
+        if (name.equals("Alice") || name.equals ("Bob") );
+        System.out.print(" Hola "  + name);
+       
 
     }
 }
